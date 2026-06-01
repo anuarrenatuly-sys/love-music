4057a6f1cae025320f7bc7ae684ddd3573f80bf0
Ануар Ренатулы <anuarrenatuly@MacBook-Air-Anuar.local>
2026-06-01 23:58:45 +0500

    fix images

--- src/data/products.js
+++ src/data/products.js
@@ -7,13 +7,13 @@ export const products = [
       color: 'Black',
       warranty: '2 жыл',
       image:
-        'https://images.unsplash.com/photo-1510915361894-db8b60106cb1?auto=format&fit=crop&w=1200&q=80',
+        'https://eldoradomusic.kz/wp-content/uploads/2020/04/FENDER-PLAYER-STRATOCASTER.jpg',
         gallery: [
-          'https://images.unsplash.com/photo-1510915361894-db8b60106cb1?auto=format&fit=crop&w=1200&q=80',
+          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTqQvhGL7nDbzZzkAnAskGsC58MAAq5ec7TA&s',
         
-          'https://images.unsplash.com/photo-1525201548942-d8732f6617a0?auto=format&fit=crop&w=1200&q=80',
+          'https://www.chamberlainmusic.com/cdn/shop/products/037-8000-500_9a484809-db8c-4180-835b-7c19275ff394.jpg?v=1621506649',
         
-          'https://images.unsplash.com/photo-1519892300165-cb5542fb47c7?auto=format&fit=crop&w=1200&q=80',
+          'https://www.bajaao.com/cdn/shop/files/fender-electric-guitars-3-color-sunburst-indian-laurel-fender-squier-affinity-series-stratocaster-electric-guitar-1210055997.jpg?v=1769755373&width=1500',
         ],
     },
     {
