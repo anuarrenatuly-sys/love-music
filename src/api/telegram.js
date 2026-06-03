d7da9ab7b3eb811e8d24f9631f5f519cc37be313
Ануар Ренатулы <anuarrenatuly@MacBook-Air-Anuar.local>
2026-06-03 14:16:23 +0500

    telegram test

--- src/api/telegram.js
+++ src/api/telegram.js
@@ -1,38 +1,22 @@
-export async function sendTelegramMessage(message) {
+export async function sendTelegramMessage() {
 
-  try {
+  const TOKEN =
+    '8720379192:AAE5UeUxxyvGFVYnI9hKnlPNm9eb_fkXJZ4'
 
-    console.log(message)
+  const CHAT_ID = '5594857511'
 
-    const TOKEN =
-      '8720379192:AAE5UeUxxyvGFVYnI9hKnlPNm9eb_fkXJZ4'
-
-    const CHAT_ID = '5594857511'
-
-    const url =
-      `https://api.telegram.org/bot${TOKEN}/sendMessage`
-
-    const response = await fetch(url, {
+  fetch(
+    `https://api.telegram.org/bot${TOKEN}/sendMessage`,
+    {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({
         chat_id: CHAT_ID,
-        text: message,
+        text: 'TEST ORDER',
       }),
-    })
-
-    const data = await response.json()
-
-    console.log(data)
-
-  } catch (error) {
-
-    console.log(error)
-
-    alert('Telegram error')
-
-  }
+    }
+  )
 
 }
